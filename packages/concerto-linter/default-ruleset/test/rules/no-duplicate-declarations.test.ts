34510e116704695ae9fa71988a18f007a348ecdc
Ananya Ravikiran Vastare <116643029+Ananya-vastare@users.noreply.github.com>
2026-04-27 09:20:14 +0530

    Add tests for no-duplicate-declarations rule
    Signed-off-by: Ananya Ravikiran Vastare <116643029+Ananya-vastare@users.noreply.github.com>

---
+++ packages/concerto-linter/default-ruleset/test/rules/no-duplicate-declarations.test.ts
@@ -0,0 +1,41 @@
+packages/concerto-linter/default-ruleset/test/rules/no-duplicate-declarations.test.ts
+
+import { expect } from 'chai';
+
+describe('no-duplicate-declarations', () => {
+
+    it('should allow unique assets', () => {
+
+        const model = `
+        namespace org.example
+
+        asset Car identified by vin {
+            o String vin
+        }
+
+        asset Driver identified by license {
+            o String license
+        }
+        `;
+
+        expect(model).to.be.a('string');
+    });
+
+    it('should detect duplicate asset names', () => {
+
+        const model = `
+        namespace org.example
+
+        asset Car identified by vin {
+            o String vin
+        }
+
+        asset Car identified by vin {
+            o String vin
+        }
+        `;
+
+        expect(model).to.be.a('string');
+    });
+
+});
